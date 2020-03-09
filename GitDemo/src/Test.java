68031998f1f658a494cdd3b242811af5415b604e
andre <andre@MOTOKO.homenet.telecomitalia.it>
2020-03-09 10:51:20 +0100

    faccio altro in Test

--- GitDemo/src/Test.java
+++ GitDemo/src/Test.java
@@ -4,6 +4,11 @@ public class Test {
 	public static void main(String[] args) {
 		// stampo a video un messaggio
 		System.out.println("Ciao da andrea");
+		
+		// faccio altro
+		int numero = 1;
+		
+		System.out.println("Numero: "+numero);
 
 	}
 
